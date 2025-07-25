a289d50a421d06580daaaedb59fa24c75ae64280
oschip <oschip@163.com>
2025-07-25 14:31:24 +0800

    Init the IP config project

---
+++ ip/core/E1/libcell_clock_gating.v
@@ -0,0 +1,18 @@
+
+module libcell_clock_gating
+(
+  input        clk_i,
+  input        en_i,
+  input        test_en_i,
+  output       clk_o
+);
+
+  reg en_latch;
+  always@* begin
+    if (!clk_i) begin
+      en_latch = en_i | test_en_i;
+    end
+  end
+  assign clk_o = en_latch & clk_i;
+
+endmodule
